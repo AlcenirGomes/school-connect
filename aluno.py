0eb18e7a55a16454e802f47fef2e04cd31253dd4
Alcenir Gomes da Costa <alcenir.g.costa@hotmail.com>
2025-02-07 23:19:17 -0300

    Criação de lista de Alunos

---
+++ aluno.py
@@ -0,0 +1,12 @@
+# Lista de Alunos
+alunos = {
+  1: {
+    'id': 1, 'nome': 'Alcenir Gomes da Costa', 'idade': 37, 'data_nascimento': '06/03/1987', 'nota_primeiro_semestre': 9, 'nota_segundo_semestre': 8, 'media_final': 8.5, 'turma_id': 1
+  },
+  2: {
+    'id': 2, 'nome': 'Luan Miranda Higino', 'idade': 20, 'data_nascimento': '30/04/2004', 'nota_primeiro_semestre': 8, 'nota_segundo_semestre': 8.5, 'media_final': 8.25, 'turma_id': 1
+  },
+  3: {
+    'id': 3, 'nome': 'Lucas Helamã', 'idade': 29, 'data_nascimento': '24/09/1996', 'nota_primeiro_semestre': 8.30, 'nota_segundo_semestre': 8.5, 'media_final': 8.4, 'turma_id': 1
+  }
+}
