8eb8dfd1c3f087b8584ba4c5dd9ba49049b2ccca
Alcenir Gomes da Costa <alcenir.g.costa@hotmail.com>
2025-02-07 23:18:07 -0300

    Criação de lista de Professores

---
+++ professor.py
@@ -0,0 +1,12 @@
+# Lista de Professores
+professores = {
+  1: {
+    'id': 1, 'nome': 'Andrea Volante', 'idade': 43,'data_nascimento': '10/01/1982', 'disciplina': 'Soft Skill', 'salario': 4500
+  },
+  2: {
+    'id': 2, 'nome': 'Gustavo Santade', 'idade': 45, 'data_nascimento': '10/01/1980', 'disciplina': 'Fundamento de Banco de Dados', 'salario': 5000
+  },
+  3:{
+    'id': 3, 'nome': 'Julio Francisco', 'idade': 36, 'data_nascimento': '07/02/1989', 'disciplina': 'Linguagem de Programação', 'salario': 4700
+  }
+}
